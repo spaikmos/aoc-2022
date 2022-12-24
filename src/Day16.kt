eb4ce06e6bb9dbd8c12fec3868f95343ecbf4112
Steve Paik <spaik@google.com>
2022-12-23 16:34:17 -0800

    Advent Day 16 solution

--- src/Day16.kt
+++ src/Day16.kt
@@ -16,7 +16,7 @@ fun main() {
         }
 
         // Find the nodes that have flowRate > 0
-        var nodesWithFlow = Node.nodes.values.filter{it -> it.flowRate > 0}.toMutableList()
+        val nodesWithFlow = Node.nodes.values.filter{it -> it.flowRate > 0}.toMutableList()
         nodesWithFlow.add(Node.nodes.get("AA")!!)
 
         // Build a map of each node with flowRate > 1
@@ -40,11 +40,8 @@ fun main() {
                 repeat(numQ) {
                     val curNode = q.removeFirst()
                     if (curNode.flowRate > 0 && i > 1) {
-                        m.put(curNode.name, i)
-                        val startName = startNode.name
-                        val destName = curNode.name
-                        val flow = curNode.flowRate
-                        println("start: $startName, dest: $destName, dist: $i, flow: $flow")
+                        m[curNode.name] = i
+                        //println("start: ${startNode.name}, dest: ${curNode.name}, dist: $i, flow: ${curNode.flowRate}")
                         nodeCnt++
                     }
                     for (nextNode in curNode.neighborMap.values) {
@@ -56,9 +53,6 @@ fun main() {
                 }
             }
             Node.flowMap.put(startNode.name, m)
-            val name = startNode.name
-            val num = m.size
-            //println("name: $name, num: $num")
         }
     }
 
@@ -67,22 +61,22 @@ fun main() {
         var numNodesExamined = 0
         var maxScore = 0
         val q = ArrayDeque<Pair<MutableList<Node>, Array<Int>>>()
-        q.add(Pair(mutableListOf(Node.nodes["AA"]!!), arrayOf(1, 0)))
+        q.add(Pair(mutableListOf(Node.nodes["AA"]!!), arrayOf(0, 0)))
         while (q.size > 0) {
             numNodesExamined++
             val curElem = q.removeFirst()
             val visitedNodes = curElem.first
             val curNode = curElem.first.last()
             val curTime = curElem.second[0]
-            val curScore = curElem.second[1] + curNode.flowRate * (31 - curTime)
+            val curScore = curElem.second[1] + curNode.flowRate * (30 - curTime)
             val neighbors = Node.flowMap[curNode.name]
             var routeEnd = true
 
             for (nextNode in Node.flowMap.keys.map{it -> Node.nodes[it]!!}) {
-                if (visitedNodes.contains(nextNode) == false) {
+                if (!visitedNodes.contains(nextNode)) {
                     // try visiting this node
                     val newTime = curTime + neighbors!![nextNode.name]!!
-                    if (newTime <= 30) {
+                    if (newTime < 30) {
                         val newList = visitedNodes.toMutableList()
                         newList.add(nextNode)
                         q.add(Pair(newList, arrayOf(newTime, curScore)))
@@ -93,27 +87,25 @@ fun main() {
 
             if (routeEnd && (maxScore < curScore)) {
                 maxScore = curScore
-                println(visitedNodes.map { it.name })
+                //println(visitedNodes.map { it.name })
             }
         }
-        println("Examined: $numNodesExamined")
+        println("Examined: $numNodesExamined nodes")
         return maxScore
     }
-
     fun part2(input: List<String>): Int {
         // Now do a BFS to maximize amount of pressure released for two people
         var numNodesExamined = 0
         var maxScore = 0
-        val q = ArrayDeque<Pair<MutableList<Node>, Array<Int>>>()
-        q.add(Pair(mutableListOf(Node.nodes["AA"]!!), arrayOf(1, 1, 0)))
+        val q = ArrayDeque<Pair<String, Array<Int>>>()
+        q.add(Pair("AA", arrayOf(0, 0, 0)))
         while (q.size > 0) {
             numNodesExamined++
             val curElem = q.removeFirst()
-            val visitedNodes = curElem.first
-            val curA = curElem.first.first()
-            val curB = curElem.first.last()
-            var timeA = curElem.second[0]
-            var timeB = curElem.second[1]
+            val visitedNodes = curElem.first.chunked(2)
+            val curA = Node.nodes[visitedNodes.first()]
+            val curB = Node.nodes[visitedNodes.last()]
+            var (timeA, timeB, curScore) = curElem.second
             val isNodeA = timeA < timeB
             var curNode = curB
             var curTime = timeB
@@ -123,54 +115,58 @@ fun main() {
                 curNode = curA
                 curTime = timeA
             }
-            val curScore = curElem.second[2] + curNode.flowRate * (27 - curTime)
-            val neighbors = Node.flowMap.get(curNode.name)
+            curScore += curNode!!.flowRate.times((26 - curTime))
+            val neighbors = Node.flowMap.get(curNode?.name)
 
-            for (nextNode in Node.flowMap.keys.map{it -> Node.nodes.get(it)!!}) {
-                if (visitedNodes.contains(nextNode) == false) {
+            for (nextNode in Node.flowMap.keys.map{it -> Node.nodes[it]!!}) {
+                if (!visitedNodes.contains(nextNode.name)) {
                     // try visiting this node
-                    val newTime = curTime + neighbors!!.get(nextNode.name)!!
-                    if (newTime <= 26) {
-                        val newList = visitedNodes.toMutableList()
+                    val destTime = neighbors!![nextNode.name]!!
+                    val newTime = curTime + destTime
+                    // The BFS grows quickly and runs out of memory.  To mitigate this, do not
+                    //  follow any paths where the destTime takes 10+ minutes.  This cuts down the
+                    //  BFS queue size and allows the computer to compute the answer.  I chose 10
+                    //  minutes because the furthest node from the origin (AA) is 9 min away.
+                    if ((newTime < 26) && (destTime < 10)) {
+                        var newString: String
                         if (isNodeA) {
-                            newList.add(0, nextNode)
+                            newString = nextNode.name + curElem.first
                             timeA = newTime
                         } else {
-                            newList.add(nextNode)
+                            newString = curElem.first + nextNode.name
                             timeB = newTime
                         }
-                        q.add(Pair(newList, arrayOf(timeA, timeB, curScore)))
+                        q.add(Pair(newString, arrayOf(timeA, timeB, curScore)))
                         routeEnd = false
                     }
                 }
             }
             if (routeEnd) {
                 if (isNodeA) {
-                    curElem.second[0] = 30
+                    timeA = 30
                 } else {
-                    curElem.second[1] = 30
+                    timeB = 30
                 }
-                if ((curElem.second[0] == 30) && (curElem.second[1] == 30)) {
+                if ((timeA == 30) && (timeB == 30)) {
                     if (maxScore < curScore) {
                         maxScore = curScore
-                        println("maxScore: $maxScore")
-                        println(visitedNodes.map{it.name})
+                        println("maxScore: $maxScore, queue size: ${q.size}")
                     }
                 } else {
                     // The other path hasn't finished so add it to the queue.
-                    q.add(curElem)
+                    q.add(Pair(curElem.first, arrayOf(timeA, timeB, curScore)))
                 }
-
             }
         }
-        println("Examined: $numNodesExamined")
+        println("Examined: $numNodesExamined nodes")
         return maxScore
     }
 
-    val input = readInput("../input/Day16e")
+
+    val input = readInput("../input/Day16")
     parseInput(input)
     println(part1(input))   // 1647
-    println(part2(input))   // Still haven't completed
+    println(part2(input))   // 2169
 }
 
 class Node(val name: String, val flowRate: Int, val neighborList: List<String>) {
@@ -185,17 +181,16 @@ class Node(val name: String, val flowRate: Int, val neighborList: List<String>) 
         }
     }
 
-    val neighborMap: MutableMap<String, Node>
+    val neighborMap: MutableMap<String, Node> = mutableMapOf()
     var seen = false
 
     init {
-        neighborMap = mutableMapOf()
-        nodes.put(name, this)
+        nodes[name] = this
         for (n in neighborList) {
-            val nNode = nodes.get(n)
+            val nNode = nodes[n]
             if (nNode != null) {
-                neighborMap.put(n, nNode)
-                nNode.neighborMap.put(name, this)
+                neighborMap[n] = nNode
+                nNode.neighborMap[name] = this
             }
         }
     }
